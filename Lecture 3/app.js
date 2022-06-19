90a9cd547852448a31c914293c683322a36d58d2
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-06-19 19:47:24 +0300

    event listener can be added, functionality needs work

--- Lecture 3/app.js
+++ Lecture 3/app.js
@@ -1,1 +1,32 @@
-console.log('hey');
+//attemptEventListenerBinding();
+changeBackGroundColorAtInterval();
+
+
+function attemptEventListenerBinding(){
+    //get items
+    let allItems = document.getElementsByClassName('container')[0]
+    .getElementsByClassName('item');
+    let allItemsAsArr = Array.from(allItems);
+    //simple way to attach event listener
+    allItemsAsArr.forEach(item=>item.addEventListener('click',()=>{
+        console.log('hey');
+    }))
+    
+}
+
+function changeBackGroundColorAtInterval(){
+    let allItems = document.getElementsByClassName('container')[0]
+    .getElementsByClassName('item');
+        
+            // setTimeout(()=>{
+            //     //try to find all items
+                
+        
+            //     let randomNumber = Math.trunc(Math.random() * 29);
+            //     let itemToChange = allItems[randomNumber];
+            //     if(itemToChange.textContent == ''){
+            //         itemToChange.style.backgroundColor = 'red';
+            //         itemToChange.classList.add('active');
+            //     }
+            // }, 1000);    
+}
